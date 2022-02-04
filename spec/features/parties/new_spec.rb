023bfe284bd400f705e0f0cedf5c3d947b4bd804
Sierra Tucker <sierra.tucker121@gmail.com>
2022-02-04 10:33:58 -0700

    Update: fix test data to check boxes

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "New Party" do
 
-  let(:user_1) { User.create!(name: "Carie Hiller", email: "carie@gmail.com")}
-  let(:user_2) { User.create!(name: "Barry Biller", email: "barry@gmail.com")}
-  let(:user_3) { User.create!(name: "Carl Hill", email: "carlos@gmail.com")}
-
   before(:each) do
-    visit new_user_movie_party_path(user_1.id, 550)
+    @user_1 = User.create!(name: "Carie Hiller", email: "carie@gmail.com")
+    @user_2 = User.create!(name: "Barry Hiller", email: "barie@gmail.com")
+    @user_3 = User.create!(name: "Carl Hiller", email: "cccarie@gmail.com")
+    visit new_user_movie_party_path(@user_1.id, 550)
   end
 
   describe "user visits movie page and sees a form to create a party" do
@@ -22,14 +21,12 @@ RSpec.describe "New Party" do
       select("2", from: "_date_3i")
       select("08 PM", from: "_time_4i")
       select("15", from: "_time_5i")
-      # within "#user-#{user_2.id}" do
-      #   check
-      # end
+      page.check(@user_2.name)
+      page.check(@user_3.name)
       click_button "Create Party"
-      expect(current_path).to eq(user_path(user_1.id))
+      expect(current_path).to eq(user_path(@user_1.id))
       expect(page).to have_content("Fight Club")
       expect(page).to have_content("You're the host!")
-
     end
 
     it "does not create a party shorter than the run time" do
@@ -39,10 +36,10 @@ RSpec.describe "New Party" do
       select("2", from: "_date_3i")
       select("08 PM", from: "_time_4i")
       select("15", from: "_time_5i")
-      # check(user_2.id)
-      # check(user_3.id)
+      page.check(@user_2.name)
+      page.check(@user_3.name)
       click_button "Create Party"
-      expect(current_path).to eq(new_user_movie_party_path(user_1.id, 550))
+      expect(current_path).to eq(new_user_movie_party_path(@user_1.id, 550))
       expect(page).to have_content("This party is too short!! Loser...")
     end
 
@@ -53,27 +50,27 @@ RSpec.describe "New Party" do
       select("2", from: "_date_3i")
       select("08 PM", from: "_time_4i")
       select("15", from: "_time_5i")
-      # check(user_2.id)
-      # check(user_3.id)
+      page.check(@user_2.name)
+      page.check(@user_3.name)
       click_button "Create Party"
-      expect(current_path).to eq(user_path(user_1.id))
+      expect(current_path).to eq(user_path(@user_1.id))
       expect(page).to have_content("February 02, 2022")
       expect(page).to have_content("8:15 PM")
       expect(page).to have_content("Fight Club")
     end
 
-    xit "displays party on the invited users dashboard" do
+    it "displays party on the invited users dashboard" do
       fill_in :duration, with: 139
       select("2022", from: "_date_1i")
       select("February", from: "_date_2i")
       select("2", from: "_date_3i")
       select("08 PM", from: "_time_4i")
       select("15", from: "_time_5i")
-      # check(user_2.id)
-      # check(user_3.id)
+      page.check(@user_2.name)
+      page.check(@user_3.name)
       click_button "Create Party"
-      expect(current_path).to eq(user_path(user_1.id))
-      visit user_path(user_2.id)
+      expect(current_path).to eq(user_path(@user_1.id))
+      visit user_path(@user_2.id)
       expect(page).to have_content("February 02, 2022")
       expect(page).to have_content("8:15 PM")
       expect(page).to have_content("Fight Club")
